dfdc203847578d9418b90f2014e74fe766b74900
KaramShounia <99362596+KaramShounia@users.noreply.github.com>
2025-03-04 16:45:25 -0500

    added vpc and public subnets

--- main.tf
+++ main.tf
@@ -9,5 +9,32 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
+}
+
+resource "aws_vpc" "MyBlog" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "MyBlog"
+  }
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id     = aws_vpc.MyBlog.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "${var.region}a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id     = aws_vpc.MyBlog.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "${var.region}b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-2"
+  }
 }
